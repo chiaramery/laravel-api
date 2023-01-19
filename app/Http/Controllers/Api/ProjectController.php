71e688395b4708d57d182af5c797dbb96fa121b0
chiaramery <meringolachiara@gmail.com>
2023-01-19 15:22:31 +0100

    Add: connection with api

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -10,8 +10,8 @@ class ProjectController extends Controller
 {
     public function index()
     {
-        // $projects = Project::all();
-        $projects = Project::with('type', 'technologies')->get();
+        $projects = Project::all();
+        // $projects = Project::with('type', 'technologies')->get();
         return response()->json([
             'success' => true,
             'results' => $projects
